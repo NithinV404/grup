ff78d9300db948539c01f0d7d9ff7e7562cf9379
Nithin V <nithin.v053@gmail.com>
2025-04-26 11:05:55 +0530

    Added simple pattern search

---
+++ src/patterns/simple.rs
@@ -0,0 +1,46 @@
+use std::cmp::max;
+
+fn badcharheuristic(s: &str) -> Vec<i32> {
+    let ascii_char = 256;
+    let mut bad_char = vec![-1; ascii_char];
+    for (i, c) in s.chars().enumerate() {
+        bad_char[c as usize] = i as i32;
+    }
+    bad_char
+}
+
+pub fn simple_pattern(text: &str, pattern: &str) -> Vec<i32> {
+    let text_bytes = text.as_bytes();
+    let pattern_bytes = pattern.as_bytes();
+    let pattern_len = pattern_bytes.len();
+    let text_len = text_bytes.len();
+    let mut skip: usize = 0;
+    let bad_char = badcharheuristic(pattern);
+    let mut index = Vec::new();
+
+    while skip <= text_len - pattern_len {
+        let mut j = pattern_len as i32 - 1;
+        while j >= 0 && text_bytes[skip + j as usize] == pattern_bytes[j as usize] {
+            j -= 1;
+        }
+
+        if j < 0 {
+            index.push(skip as i32);
+            skip += if skip + pattern_len < text_len {
+                let bad_char_value = bad_char[text_bytes[skip + pattern_len] as usize];
+                if bad_char_value < 0 {
+                    pattern_len
+                } else {
+                    pattern_len - bad_char_value as usize
+                }
+            } else {
+                1
+            };
+        } else {
+            let text_char = text_bytes[skip + j as usize] as usize;
+            let shift = max(1, j - bad_char[text_char]) as usize;
+            skip += shift;
+        }
+    }
+    index
+}
